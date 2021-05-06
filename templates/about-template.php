85d2fc560a92104b27b6419c6b8d3df47390155c
jerryrold <83703535+jerryrold@users.noreply.github.com>
2021-05-06 19:10:08 +0800

    jerry's

---
+++ templates/about-template.php
@@ -0,0 +1,12 @@
+<?php
+/* Template Name: About */
+get_header();
+?>
+
+Wake up and smell the opportunity! Do you make a mean cup of coffee? 
+A love for quality coffee and a desire to create a warm, by jerry's coffee shop 
+or cafe could blend to create the newest caffeine hub for your community. 
+Plan for success with our coffee shop business sample plans.
+
+
+<?php get_footer(); ?>
